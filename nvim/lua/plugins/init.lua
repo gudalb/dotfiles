0705e1f112fea7f2926842100291d640c68fc4e9
Albin Gudmundsson <albin.gudmundsson@nordicstation.com>
2025-08-27 14:22:32 +0200

    wider nvimtree

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -416,6 +416,9 @@ return {
     'nvim-tree/nvim-tree.lua',
     config = function()
       require('nvim-tree').setup {
+        view = {
+          width = 40,
+        },
         on_attach = function(bufnr)
           local api = require 'nvim-tree.api'
 
