3fef01696c894b2fa6df4f5c681defa0f1991d6c
Albin Gudmundsson <albin.gudmundsson@nordicstation.com>
2025-11-06 15:02:04 +0100

    rm old hypr

--- hypr/scripts/setup-monitors.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if hyprctl monitors | grep -q "eDP-1"; then
-    # Laptop setup
-    hyprctl keyword monitor "eDP-1,highrr,auto,1.25"
-    hyprctl keyword monitor ",preferred,auto,1"
-else
-    # Desktop setup
-    hyprctl keyword monitor ",highrr,auto,auto"
-fi
