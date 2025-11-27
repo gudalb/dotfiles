5731c85ee0a97804683eb3fbb827bf46847f134a
Albin Gudmundsson <albin.gudmundsson@nordicstation.com>
2025-11-27 19:51:35 +0100

    auto reload files

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,6 +26,23 @@ vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.confirm = true
 vim.go.background = 'dark'
+
+-- Auto-reload files when changed externally
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold', 'CursorHoldI' }, {
+  pattern = '*',
+  callback = function()
+    if vim.fn.mode() ~= 'c' then
+      vim.cmd('checktime')
+    end
+  end,
+})
+vim.api.nvim_create_autocmd('FileChangedShellPost', {
+  pattern = '*',
+  callback = function()
+    vim.notify('File changed on disk. Buffer reloaded.', vim.log.levels.WARN)
+  end,
+})
 vim.g.snacks_animate = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
