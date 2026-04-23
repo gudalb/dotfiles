6b5b11cfbe19312c7123f11c93220f6ef7bf5e7c
Albin Gudmundsson <albin.gudmundsson@nionit.com>
2026-04-23 06:22:10 +0200

    remove lazygit

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -428,22 +428,6 @@ return {
     },
   },
   {
-    'kdheepak/lazygit.nvim',
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
-    },
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    keys = {
-      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-    },
-  },
-  {
     'mfussenegger/nvim-dap',
     dependencies = {
       'rcarriga/nvim-dap-ui',
